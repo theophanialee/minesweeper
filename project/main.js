c52fd74abc013c668593a7699cda9e43b79c2508
Theophania <pha.randomacc@gmail.com>
2023-05-25 00:06:03 +0800

    wed

--- project/main.js
+++ project/main.js
@@ -43,6 +43,9 @@ function handleStart () {
 }
 
 function handleSetup() {
+  gameElements.flags =0
+  gameBoard.addEventListener ("click", handleClick);
+  gameBoard.addEventListener ("contextmenu", handleFlagging);
   // set up table as board size
   //create mines and spaces in seperate arrays > combine > randomise > split
   // backgroundMusic.play();
@@ -59,12 +62,28 @@ render ();
 }
 
 function handleClick (e) {
+  gameBoard.addEventListener ("click", handleClick);
   let clickCell = e.target;
   console.log(clickCell)
-  clickCell.classList.remove(gameElements.state)
-  let x = parseInt(clickCell.id[0]);
-  let y = parseInt(clickCell.id[2]);
+  clickCell.classList.remove(gameElements.state);
+  let x = "";
+  let y = "";
+  if (clickCell.id.length === 3) {
+   x = parseInt(clickCell.id[0]);
+   y = parseInt(clickCell.id[2]);
+  } else if (clickCell.id.length === 4 && clickCell.id[1] === "-") {
+    x = parseInt(clickCell.id[0]);
+    y = parseInt(clickCell.id[2] + clickCell.id[3]);
+  } else if (clickCell.id.length === 4 && clickCell.id[2] === "-") {
+    x = parseInt(clickCell.id[0] + clickCell.id[1]);
+    y = parseInt(clickCell.id[3]);
+  }
+  else if (clickCell.id.length === 5) {
+    x = parseInt(clickCell.id[0] + clickCell.id[1]);
+    y = parseInt(clickCell.id[3] + clickCell.id[4]);
+  }
   console.log("x-y coord id: ",clickCell.id)
+  console.log(gameElements.countArr[x][y]);
   clickCell.classList.add(`N${gameElements.countArr[x][y]}`)
   //put into render function?
   if (gameElements.countArr[x][y] === "bomb") {
@@ -75,6 +94,9 @@ function handleClick (e) {
     setTimeout(function() {
       alert("YOU LOSE! 😵");
     }, 200);
+    clickCell.classList.add(`Nbomb`)
+    gameBoard.removeEventListener ("click", handleClick);
+    gameBoard.removeEventListener ("contextmenu", handleFlagging);
     // to open all bombs in bombArr
      for (let i=0; i < gameElements.size; i++) {
       for (let j = 0; j < gameElements.size; j++) {
@@ -87,15 +109,17 @@ function handleClick (e) {
     }
   } else if (gameElements.countArr[x][y] === 0) {
     clickCell.innerText = (`${gameElements.countArr[x][y]}`);
-    floodNeighbour (x,y);
+    // floodNeighbour (x,y);
    } else { 
         clickCell.innerText = (`${gameElements.countArr[x][y]}`);
       }
-      console.log ("total: ",countBombs(gameElements.bombArr,x,y))
+      console.log ("total: ",countBombs(gameElements.bombArr,x,y));
+      renderAltWin();
     }
         
 
 function handleFlagging (e) {
+  gameBoard.addEventListener("contextmenu", handleFlagging);
   let flagCell = e.target;
   e.preventDefault()
   // console.log (flagCell.classList.value);
@@ -115,6 +139,10 @@ function handleFlagging (e) {
     gameElements.flags-=1;
     flagCount.innerText = `Flags: ${gameElements.mines - gameElements.flags}`;
   }
+  let flagEl = document.querySelectorAll(".flag")
+  if (flagEl.length >= gameElements.mines) {
+    gameBoard.removeEventListener("contextmenu", handleFlagging);
+  }
   checkWin (flagCell, x,y);
   console.log("flags: ", gameElements.flags);
   }
@@ -123,8 +151,8 @@ function handleFlagging (e) {
 /* ----- render functions -----*/
 function render() {
 renderNewBoard();
-renderResult();
 renderScreen();
+// renderAltWin ();
 };
 
 function renderScreen() {
@@ -150,10 +178,38 @@ function renderNewBoard () {
     }
   }
 }
-function renderResult () {
-  
+
+function renderAltWin () {
+  const hiddenCells = document.querySelectorAll(".hidden");
+  console.log("hidden: ", hiddenCells.length)
+  if (hiddenCells.length === gameElements.mines) {
+    resultMessage.innerText = "🥳";
+    winMusic.play();
+    // backgroundMusic.pause();
+    setTimeout(function() {
+      alert("YOU WON! 🥳");
+    }, 200);
+    gameBoard.removeEventListener ("click", handleClick);
+    gameBoard.removeEventListener ("contextmenu", handleFlagging);
+  }
 }
 
+function checkWin (flagCell,x,y) {
+  if (gameElements.bombArr[x][y] === "bomb" && flagCell.classList.contains("flag")) {
+    gameElements.correctFlag++
+  }
+  if (gameElements.correctFlag === gameElements.mines) {
+    resultMessage.innerText = "🥳";
+    winMusic.play();
+    // backgroundMusic.pause();
+    setTimeout(function() {
+      alert("YOU WON! 🥳");
+    }, 200);
+    gameBoard.removeEventListener ("click", handleClick);
+    gameBoard.removeEventListener ("contextmenu", handleFlagging);
+  }
+  console.log ("correct: ", gameElements.correctFlag)
+}
 
 /* ----- functions -----*/
 
@@ -179,6 +235,7 @@ function rowArray(array, chunkSize) {
 }
 
 function countBoard () {
+  gameElements.countArr = [];
   for (let i=0; i < gameElements.size; i++) {
     let countRow = [];
     for (let j = 0; j < gameElements.size; j++) {
@@ -219,24 +276,24 @@ function countBombs (arr,x,y) {
 }
 
 
-function checkWin (flagCell,x,y) {
-  if (gameElements.bombArr[x][y] === "bomb" && flagCell.classList.contains("flag")) {
-    gameElements.correctFlag++
-  }
-  if (gameElements.correctFlag === gameElements.mines) {
-    resultMessage.innerText = "🥳";
-    winMusic.play();
-    // backgroundMusic.pause();
-    setTimeout(function() {
-      alert("YOU WON! 🥳");
-    }, 200);
-  }
-  console.log ("correct: ", gameElements.correctFlag)
-}
-
 function floodNeighbour (x,y) {
+  //to locate cells surrounding x,y and unhide them
+  // conditions to unhide surrounding cells: 
+  // if its not a bomb (space or count)
+  // re-run the function on surrounding cell if that surrounding cell = 0
+  // keep the function running until no more surrounding cells = 0 or already revealed
+  // if no class "hidden" (ie already revealed), move to the next adjacent cell
+  
+  // unhide function: 
+  // remove class "hidden"
+  // add class "N arr[x][y]" (x-y is id of cellEl)
+
+  //recurssion: 
+  // [1] condition to end the function
+  // [2] x-y should always be different
   for (let i = -1; i <= 1; i++) {
     for (let j = -1; j <= 1; j++) { 
+      let neighboutEl = document.getElementById(`${[x+i]}-${[y+j]}`);
       if (i == 0 && j == 0) {
         continue;
       }
@@ -246,13 +303,15 @@ function floodNeighbour (x,y) {
       if ((y+j) < 0 || (y+j) > gameElements.size) {
         continue;
       }
+      // if (!neighboutEl.classList.contains("hidden")) { // does not contain hidden ie revealed
+      //   continue;
+      // }
       else {
-        let neighboutEl = document.getElementById(`${[x+i]}-${[y+j]}`);
         neighboutEl.innerText = (`${gameElements.countArr[x+i][y+j]}`);
         neighboutEl.classList.remove(gameElements.state);
         neighboutEl.classList = (`N${gameElements.countArr[x+i][y+j]}`);
+ 
       }
     }
   }
 }
-
